aa80e9433cb99d449cfa464c55bdea4125646056
stelladream <nykim@hansung.ac.kr>
2025-03-21 19:35:41 +0900

    add spring form functionality

--- src/main/java/kr/ac/hansung/cse/controller/OfferController.java
+++ src/main/java/kr/ac/hansung/cse/controller/OfferController.java
@@ -1,12 +1,17 @@
 package kr.ac.hansung.cse.controller;
 
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
 import kr.ac.hansung.cse.model.Offer;
 import kr.ac.hansung.cse.service.OfferService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -23,4 +28,32 @@ public class OfferController {
 
         return "offers";
     }
+
+    @GetMapping("/createoffer")
+    public String createOffer(Model model) {
+        model.addAttribute("offer", new Offer());
+        return "createoffer";
+    }
+
+    @PostMapping("docreate")
+    public String doCreate(Model model, @Valid Offer offer, BindingResult result) {
+        //System.out.println(offer);
+
+        if(result.hasErrors()) {
+            System.out.println("== Form data does not validated ==");
+
+            List<ObjectError> errors = result.getAllErrors();
+
+            for(ObjectError error:errors) {
+                System.out.println(error.getDefaultMessage());
+            }
+
+            return "createoffer";
+        }
+
+        //Controller -> Service -> Dao
+        offerService.insertOffer(offer);
+        return "offercreated";
+    }
+
 }
